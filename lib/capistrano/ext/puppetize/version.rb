37988fc20d46f2775d53598be3cc401f20898e7d
PeterWuMC <peter.wu@simplybusiness.co.uk>
2014-08-29 12:57:22 +0100

    Bump version

--- lib/capistrano/ext/puppetize/version.rb
+++ lib/capistrano/ext/puppetize/version.rb
@@ -2,7 +2,7 @@ module Capistrano
   module Ext
     module Puppetize
       module Version
-        MAJOR = 0
+        MAJOR = 1
         MINOR = 4
         TINY = 0
         STRING= [MAJOR, MINOR, TINY].join('.')
